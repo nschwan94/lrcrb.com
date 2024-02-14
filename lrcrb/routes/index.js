38e9379733e058d51f07557559a7af4d601068c2
Nathan Alexander Schwan <nathan.schwan.94@gmail.com>
2024-02-14 06:34:49 -0700

    added nodemailer and integrated with longlivelimerick@gmail account

--- lrcrb/routes/index.js
+++ lrcrb/routes/index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var nodemailer = require('nodemailer');
 var router = express.Router();
 
 /* GET home page. */
@@ -25,6 +26,43 @@ router.get("/white", (req, res) => {
 router.get("/blue", (req, res) => {
   res.render('users')
 });
+
+
+// nodemailer integration
+// Configure Nodemailer transporter (replace with your email credentials)
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  port: 587,
+  secure: false, // Use true for TLS, false for STARTTLS
+  auth: {
+    user: 'longlivelimerick@gmail.com',
+    pass: 'coyc sxwa tzja gmav'
+  }
+});
+
+// Handle POST request from email submission form
+router.post('/submit-email', (req, res) => {
+  const email = req.body.email;
+
+  // Send email using Nodemailer
+  const mailOptions = {
+    from: 'Lime Rick <longlivelimerick@gmail.com>',
+    to: [email],
+    subject: 'Email submitted',
+    text: 'Thank you for your interest in lrcrb.com at this time. There is much going on, and I invite you to follow @lrcrb_ldl on X.com for updates. Stay Limey and Make Them Pucker!'
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error(error);
+      res.status(500).send({ data: 'Error sending email'});
+    } else {
+      console.log(`Email sent: ${info.response}`);
+      res.send({ data: 'Email submitted successfully!'});
+    }
+  });
+});
+
 
 
 
